79e74304976af446bd004f40cea7d69e5fa9078f
KedarPanchal <kedar.t.panchal@outlook.com>
2024-03-24 21:06:44 -0500

    Fix piping issue

--- src/main/java/com/kpanchal/lspp/tree/FileTreeWalker.java
+++ src/main/java/com/kpanchal/lspp/tree/FileTreeWalker.java
@@ -18,18 +18,36 @@ public class FileTreeWalker {
         listFiles(this.head, this.toDepth, "");
     }
 
-    private void listFiles(FileTree.FileTreeNode treeHead, int depth, String symbol) {
+    private void listFiles(FileTree.FileTreeNode treeHead, int depth, String symbol, boolean diff) {
         for (int i = this.toDepth - 1; i > depth; i--) {
             System.out.print("    ");
         }
         System.out.print(symbol + treeHead.toString());
         for (FileTree.FileTreeNode child : treeHead.getChildren()) {
-            String nextSymbol = "├── ";
-            if (child.equals(treeHead.getChildren().getLast())) {
-                nextSymbol = "└── ";
-            }
+            String nextSymbol = child.equals(treeHead.getChildren().getLast()) ? "└── " : "├── "; 
             System.out.println();
             listFiles(child, depth - 1, nextSymbol);
+        }
+    }
+
+    private void listFiles(FileTree.FileTreeNode treeHead, int depth, String symbol) {
+        if (depth == 0) {
+            return;
+        }
+
+        if (!symbol.isEmpty()) {
+            System.out.print(symbol.substring(0, symbol.length() - 4).replace("├── ", "│   ").replace("└── ", "    ") + symbol.substring(symbol.length() - 4) + treeHead.toString());
+        } else {
+            System.out.print(treeHead);
+        }
+
+        for (FileTree.FileTreeNode child : treeHead.getChildren()) {
+            System.out.println();
+            if (child.equals(treeHead.getChildren().getLast())) {
+                this.listFiles(child, depth - 1, symbol + "└── ");
+            }  else {
+                this.listFiles(child, depth - 1, symbol + "├── ");
+            }
         }
     }
 }
