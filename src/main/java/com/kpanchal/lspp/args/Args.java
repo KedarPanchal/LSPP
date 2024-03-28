d710bd41a901d0d49d8dd1a58988ab0d8f040f69
KedarPanchal <kedar.t.panchal@outlook.com>
2024-03-28 13:52:11 -0500

    Add parameter descriptions

--- src/main/java/com/kpanchal/lspp/args/Args.java
+++ src/main/java/com/kpanchal/lspp/args/Args.java
@@ -5,22 +5,25 @@ import java.nio.file.Path;
 import com.beust.jcommander.Parameter;
 
 public class Args {
-    @Parameter(converter=PathConverter.class)
+    @Parameter(description="The name of the file to list all subfiles and subdirectories from. If left blank, this is the current working directory.", converter=PathConverter.class)
     private Path directory = Path.of(System.getProperty("user.dir"));
 
-    @Parameter(names={"-d", "--depth"})
+    @Parameter(names={"-d", "--depth"}, description="The number of layers of files that will be listed. If left blank or given a value of 0, all subfiles and subdirectories will be listed.")
     private Integer depth = null;
 
-    @Parameter(names={"-s", "--search"})
-    private String fileName;
+    @Parameter(names={"-s", "--search"}, description="The file to search for. Only the file's parent directories will be displayed.")
+    private String fileName = null;
 
-    @Parameter(names={"-c", "--charset"}, converter=CharsetConverter.class)
+    @Parameter(names={"-c", "--charset"}, description="""
+            The character set to use when displaying the tree. The current character sets available are:
+            1. box
+            2. ascii""", converter=CharsetConverter.class)
     private CharsetEnum charset = CharsetEnum.BOX;
 
-    @Parameter(names={"-h", "--help"}, help=true)
+    @Parameter(names={"-h", "--help"}, description="Lists the functionality of each of this program's parameters.", help=true)
     private boolean help = false;
 
-    @Parameter(names={"-v", "--version"})
+    @Parameter(names={"-v", "--version"}, description="Gets the version of this program.")
     private boolean version = false;
 
     public Path getPath() {
