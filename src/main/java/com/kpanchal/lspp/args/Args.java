3680014a0c58c2bcfb7a009e722c9f125e77095c
KedarPanchal <kedar.t.panchal@outlook.com>
2024-03-28 13:53:25 -0500

    Added depth validation functionality

--- src/main/java/com/kpanchal/lspp/args/Args.java
+++ src/main/java/com/kpanchal/lspp/args/Args.java
@@ -8,7 +8,7 @@ public class Args {
     @Parameter(description="The name of the file to list all subfiles and subdirectories from. If left blank, this is the current working directory.", converter=PathConverter.class)
     private Path directory = Path.of(System.getProperty("user.dir"));
 
-    @Parameter(names={"-d", "--depth"}, description="The number of layers of files that will be listed. If left blank or given a value of 0, all subfiles and subdirectories will be listed.")
+    @Parameter(names={"-d", "--depth"}, description="The number of layers of files that will be listed. If left blank or given a value of 0, all subfiles and subdirectories will be listed.", validateWith=DepthValidator.class)
     private Integer depth = null;
 
     @Parameter(names={"-s", "--search"}, description="The file to search for. Only the file's parent directories will be displayed.")
@@ -17,7 +17,8 @@ public class Args {
     @Parameter(names={"-c", "--charset"}, description="""
             The character set to use when displaying the tree. The current character sets available are:
             1. box
-            2. ascii""", converter=CharsetConverter.class)
+            2. ascii""",
+            converter=CharsetConverter.class)
     private CharsetEnum charset = CharsetEnum.BOX;
 
     @Parameter(names={"-h", "--help"}, description="Lists the functionality of each of this program's parameters.", help=true)
