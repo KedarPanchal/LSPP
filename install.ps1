e06e4810d81d17baeb8e4d682718b10fa5671b9e
KedarPanchal <kedar.t.panchal@outlook.com>
2024-04-23 12:56:32 -0500

    Update installation script

--- install.ps1
+++ install.ps1
@@ -1,13 +1,14 @@
 $CommandParent = Split-Path -Parent $PSCommandPath
 Push-Location $CommandParent
 
-mvn clean package
-mvn -Pnative package
-$MovePath = Read-Host "Please enter the installation folder (enter nothing to install here): "
-
+$MovePath = Read-Host "Please enter the installation folder (enter nothing to install here)"
 if (!$Movepath) {$MovePath = $CommandParent}
 
+mvn clean package
+mvn -Pnative package
+
 Move-Item -Path "target\lspp.exe" -Destination $MovePath
+
 [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User) + ";" + $MovePath + "\lspp.exe", [System.EnvironmentVariableTarget]::User)
 
 Pop-Location
