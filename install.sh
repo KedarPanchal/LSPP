a82ab849e302cc84a6d58087a486e14cedd2e899
KedarPanchal <kedar.t.panchal@outlook.com>
2024-05-06 21:11:49 -0500

    Fix bug with lspp executable location in install script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ Command_Path="$(dirname "$(readlink -f "${BASH_SOURCE}")")";
 pushd "$Command_Path" || (echo "Installation failed" && exit);
 
 # Read installation path of lspp from user
-read -p "Please enter the installation folder (enter nothing to install at $Command_Path)" Move_Path;
+read -p "Please enter the installation folder (enter nothing to install at $Command_Path): " Move_Path;
 if [ -z "$Move_Path" ]; then
   Move_Path="$Command_Path";
 fi
@@ -15,7 +15,7 @@ mvn clean package;
 mvn -Pnative package;
 
 # Move the lspp executable to the path specified by the user
-mv lspp "$Move_Path";
+mv target/lspp "$Move_Path";
 
 # Return to the original directory
 popd || (echo "Installation completed, failed to return to original directory" && exit);
